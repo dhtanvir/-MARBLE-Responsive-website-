6a656d79de6b4a6d96e26509070f9ca9cfe0f826
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2022-01-21 22:17:25 +0600

    Add files via upload

---
+++ MARBEL.js
@@ -0,0 +1,28 @@
+ 
+
+/* activate wow.js ===== Start===========  */
+new WOW().init();
+
+wow = new WOW({
+     boxClass: 'wow', // default
+     animateClass: 'animated', // default
+     offset: 0, // default
+     mobile: true, // default
+     live: true // default
+ })
+wow.init();
+ 
+/*====== activate wow.js == End ========= */
+
+
+
+
+
+
+
+
+
+
+
+
+
